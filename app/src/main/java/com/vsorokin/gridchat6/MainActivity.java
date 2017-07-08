49dff217d5437b2e93eeabd67ca4bbe7de3d4cde
Valery Sorokin <valery.sorokin@actum.cz>
2017-07-08 18:38:22 +0200

    repo pickup

---
+++ app/src/main/java/com/vsorokin/gridchat6/MainActivity.java
@@ -0,0 +1,39 @@
+package com.vsorokin.gridchat6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView messages;
+    EditText message;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button =  (Button)  findViewById(R.id.button);
+        message =  (EditText)  findViewById(R.id.message);
+        messages =  (TextView)  findViewById(R.id.Messages);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                message.getEditableText();
+                messages.setText(message.getEditableText());
+            }
+        });
+    }
+
+    protected void sendMessage(View view){
+        String text = message.getEditableText().toString();
+        messages.setText(text);
+    }
+}
